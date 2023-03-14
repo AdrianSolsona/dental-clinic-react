e19ca79d453dbc20dbf00bbd5aaf92bae38675c1
solsona1008 <solsona1008@gmail.com>
2023-03-14 20:54:21 +0100

    Feat: Modify the structure and data related to the login and add it to the login layout.

--- src/layout/login/login.jsx
+++ src/layout/login/login.jsx
@@ -1,14 +1,39 @@
 
 import { Footer } from "../../components/Footer/Footer"
-import FormLogin from "../../components/FormLogin/FormLogin"
 import { NavBar } from "../../components/Navbar/NavBar"
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './Login.css'
 
 
 export const Login = () => {
     return (
         <>
         <NavBar/>
-        <FormLogin/>
+        <div className='all-form'>
+      <Form className='form-info'>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <div className='container-icon'>
+        <i class="bi bi-person-workspace icon-login"></i>
+        </div>
+          <Form.Control className='input-form' type="email" placeholder="Email" />
+          <Form.Text className="text-muted">
+            Tienes que introducir caracteres válidos.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control className='input-form' type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        </Form.Group>
+        <div className='btn-container'>
+          <Button className='btn-login' variant="primary" type="submit">
+            Login
+          </Button>
+        </div>
+      </Form>
+    </div>
         <Footer/>
         </>
     )
