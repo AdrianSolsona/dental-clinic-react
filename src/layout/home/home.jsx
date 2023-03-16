090ef09ae2c118c248fb1e0fdbcd991281c9d85e
adrian salona <Adrian>
2023-03-16 22:01:27 +0100

    Feat: the responsive content at the beginning of the home page is modified.

--- src/layout/home/home.jsx
+++ src/layout/home/home.jsx
@@ -18,31 +18,37 @@ export const Home = () => {
         <>
         <NavBar className='navigator'/>
         <div className='cont-discount'>
-            <div className='picture-people'>
-                <img src= {peopleSunrise} alt="" />
-            </div>
-            <div className='discount'>
-                <p className='price'>200€</p>
-                <p className='of-discount'>de descuento</p>
-                <p className='treatment'>en tu tratamiento de ortodoncia invisible o implante + corona</p>
-                <div className='button-discount'>
-                    <Button className='appointment-free'>Pide tu cita gratis</Button>
-                    <Button className='information'>Información</Button>
-                </div>
-            </div>
+            <Container>
+                <Row>
+                    <Col xs={12} md={12} lg={6} xl={6} xxl={6} className='col-discount'>
+                        <div className='picture-people'>
+                            <img src= {peopleSunrise} alt="" />
+                        </div>
+                    </Col>
+                    <Col xs={12} md={12} lg={12} xl={6} xxl={6} className='col-discount'>
+                        <div className='discount'>
+                            <p className='price'>200€</p>
+                            <p className='of-discount'>de descuento</p>
+                            <p className='treatment'>en tu tratamiento de ortodoncia invisible o implante + corona</p>
+                            <div className='button-discount'>
+                                <Button className='appointment-free'>Pide tu cita gratis</Button>
+                                <Button className='information'>Información</Button>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
         <div className='clinic-vlc'>
             <div className='clinic-ubication'>
                 <div>
                     <img src={clinicUbication} alt="" />
                 </div>
-                <div>
-                    <div className='text-ubication'>
-                        <p className='your-clinic'>Tu clinica dental en valencia</p>
-                        <p className='near-you'>Factordent es una compañía dental estable y consolidada que cuenta con una gran red de centros cerca de ti para ayudarte.</p>
-                        <Button className='know-btn'>¿Vienes a conocernos?</Button>
-                    </div>
-                </div>
+            </div>
+            <div className='text-ubication'>
+                <p className='your-clinic'>Tu clinica dental en valencia</p>
+                <p className='near-you'>Factordent es una compañía dental estable y consolidada que cuenta con una gran red de centros cerca de ti para ayudarte.</p>
+                <Button className='know-btn'>¿Vienes a conocernos?</Button>
             </div>
         </div>
         <div className='confidence-all'>
