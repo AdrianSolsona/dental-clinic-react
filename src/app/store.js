8ead7190e86f62143ecd655eaa0c7949307d808b
adrian salona <Adrian>
2023-03-20 20:04:43 +0100

    Feat: full persistence functionality is added for redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../layout/userSlice';
 import detailSlice from '../layout/detailSlice';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 
+import thunk from 'redux-thunk';
+
+
+const reducers = combineReducers({
+    user: userSlice,
+    details: detailSlice
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export default configureStore({
-    reducer: {
-        user: userSlice,
-        detail: detailSlice
-    }
+
+    reducer: persistedReducer,
+    middleware: [thunk]
 });
