02bce2d41488bc93d13b7e637a1d8184ca8edc91
adrian salona <Adrian>
2023-03-26 19:13:19 +0200

    Feat: views for the display of all system users by the administrator

--- src/layout/users/Users.jsx
+++ src/layout/users/Users.jsx
@@ -1,14 +1,12 @@
 import React, {useState, useEffect} from 'react'
 import './Users.css';
-
 import { bringUsers } from '../../services/apiCalls';
-
-//Conexion a REDUX
 import { useSelector } from "react-redux";
 import { userData } from "../userSlice";
-import { addChoosen } from '../detailSlice';
-import { useDispatch } from "react-redux";
-import { useNavigate } from 'react-router-dom';
+import { NavBar } from '../../components/Navbar/NavBar';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Footer } from '../../components/Footer/Footer';
+
 
 
 export const Users = () => {
@@ -16,9 +14,6 @@ export const Users = () => {
     const [users, setUsers] = useState([]);
 
     const ReduxCredentials = useSelector(userData);
-
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
 
     useEffect(()=>{
         //console.log(users.length)
@@ -37,49 +32,64 @@ export const Users = () => {
 
     },[users])
 
-    const selected = (persona) => {
-        
-        
-        dispatch(addChoosen({ choosenObject: persona }))
-
-        setTimeout(()=>{
-            navigate("/detail");
-        },500)
-    }
-
      return (
-         <div className='usersDesign'>
-
+        <>
+        <NavBar/>
+        <div className='your-appointments'>Todas los usuarios</div>
+        <Container>
             { users.length > 0 ? 
                 
-                (<div>
+                (<Row>
 
                     {
                         users.map(
                             persona => {
                                 return (
-                                    <div 
-                                        onClick={()=>selected(persona)} 
-                                        key={persona.id}>
-
-                                        {persona.name}
-                                        hola
-
+                                    <Col key={persona.id} sm="12" md="6" lg="6" xl="6" xxl="4">
+                                    <div className='container-int'>
+                                        <div
+                                        className= "cont-treatment-1"
+                                        >
+                                        <div>
+                                        <div className='icon-1'><i class="bi bi-journal-text"></i></div>
+                                        </div>
+                                        <div className='treatment-line'>
+                                        <div className='line-1'>Username:</div><div> {persona.username}</div>
+                                        </div>
+                                        <div className='treatment-line'>
+                                        <div className='line-1'>Nombre:</div><div> {persona.name}</div>
+                                        </div>
+                                        <div className='treatment-line'>
+                                        <div className='line-1'>Apellido:</div><div> {persona.surname}</div>
+                                        </div>
+                                        <div className='treatment-line'>
+                                        <div className='line-1'>Email:</div><div> {persona.email}</div>
+                                        </div>
+                                        <div className='treatment-line'>
+                                        <div className='line-1'>Teléfono:</div><div> {persona.phone}</div>
+                                        </div>
+                                        <div className='treatment-line'>
+                                        <div className='line-1'>Dirección:</div><div> {persona.address}</div>
+                                        </div>
+                                        <div className='treatment-line'>
+                                        <div className='line-1'>Codigo postal:</div><div> {persona.postcode}</div>
+                                        </div>
+                                        <div className='treatment-line'>
+                                        <div className='line-1'>Género:</div><div> {persona.gender}</div>
+                                        </div>
+                                        </div>
                                     </div>
+                                </Col>
                                 )
                             }
                         )
                     }
-
-
-                </div>)
-
+                </Row>)
                 : 
-
-                (<div>ESTAN VINIENDO</div>)
-
+                (<div className='date-confirm'>CARGANDO TODAS LOS USUARIOS...</div>)
             }
-
-         </div>
+        </Container>
+        <Footer/>
+        </>
      )
 }
