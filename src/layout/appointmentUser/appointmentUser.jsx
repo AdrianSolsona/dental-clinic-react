df773171d6201cf61d1450c94089af90ce226ab5
adrian salona <Adrian>
2023-03-26 19:10:00 +0200

    Feat: views are made for the visualization of the personal data of each user according to their id

--- src/layout/appointmentUser/appointmentUser.jsx
+++ src/layout/appointmentUser/appointmentUser.jsx
@@ -1,155 +1,129 @@
-/*import React, { useEffect, useState } from 'react'
-import { Spinner } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { bringAppointments } from '../../services/apiCalls';
-import { userData } from '../userSlice';
-
-export const AppointmentsAsClient = () => {
-
-  const [appointment, setAppointment] = useState([]);
-
-  const credentialsRdx = useSelector(userData)
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-
-  useEffect(() => {
-
-    if(appointment.length === 0){
-      bringAppointments(credentialsRdx.credentials.token)
-        .then(
-          result => {
-            console.log(result)
-  
-            setAppointment(result.data)
-          }
-        )
-        .catch(error => console.log(error))
-    }
-
-  },[appointment])
-
-  // const selected = (user) => {
-    
-  //   dispatch(addChoosen({ choosenOject: user}))
-  //   console.log({ choosenOject: user})
-  //   setTimeout(() => {
-  //     navigate('/profile');
-  //   }, 500)
-  // }
-
-
-
-  return (
-    <div>AppointmentsAsClient
-
-
-{ appointment.length > 0 ? 
-      (<div className="cardsContainer">
-        {
-          appointment.map(
-            info => {
-              return (
-          
-                  <div
-                    className= "userCardDesign"
-                    key={info.id}>
-                    {info.date}
-                  </div>
-             
-              )
-            }
-          )
-
-        }  
-        </div>)
-        
-        :
-
-        ( <Spinner animation="border" variant="primary" />)
-      
-      }
-    </div>
-  )
-}*/
-
 import React, { useEffect, useState } from 'react'
-import { Spinner } from 'react-bootstrap';
+import { Navbar, Spinner } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { bringAppointments } from '../../services/apiCalls';
-import { addChoosenAppointment } from '../appointmentsSlice';
+import { bringAppointments, deleteMyAppointment } from '../../services/apiCalls';
+import { addChoosenAppointment, appointmentData } from '../appointmentsSlice';
 import { userData } from '../userSlice';
-import './appointmentUser.css'
+import "./appointmentUser.css"
+import { Col, Container, Row } from 'react-bootstrap';
+import { NavBar } from '../../components/Navbar/NavBar';
+import { Footer } from '../../components/Footer/Footer';
+import Moment from 'moment';
 
 export const AppointmentsAsClient = () => {
-
   const [appointments, setAppointments] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   const credentialsRdx = useSelector(userData)
-  
-
+  console.log(credentialsRdx)
+  const appoimentSelectedRdx = useSelector(appointmentData)
+  let params = appoimentSelectedRdx.choosenAppointment.id
+  console.log(params);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   useEffect(() => {
-
     if(appointments.length === 0){
+      setTimeout(() => {
       bringAppointments(credentialsRdx.credentials.token)
         .then(
           result => {
-            console.log(result)
-            console.log("hola")
-  
-            setAppointments(result.data)
+            setLoading(false);
+            console.log(result.data)
+            if(result.data.length === 0){
+              return
+            }
+            setAppointments(result.data);
           }
         )
         .catch(error => console.log(error))
+      }, 500)
     }
-
   },[appointments])
   // const appointmentRdx = useSelector(appointmentData)
-  // console.log(appointmentRdx);
+  console.log(appointments);
   const appointmentSelected = (appointment) => {
-
-    dispatch(addChoosenAppointment({ choosenAppointment: appointment}))
-    console.log({ choosenAppointment: appointment})
+    dispatch(addChoosenAppointment({ choosenAppointment: appointment }))
+    console.log(addChoosenAppointment({ choosenAppointment: appointment }))
     setTimeout(() => {
       navigate('/modify/appointment');
-    }, 500)
-  
+    }, 1000)
   }
+  // console.log( appointments[0].id)
+  const deleteApp =  async (appointment) =>{
+    // if(appointments.length > 0) {
+    await deleteMyAppointment(  appointment.id,credentialsRdx.credentials.token)
+    setAppointments(prevAppointments => prevAppointments.filter(a => a.id !== appointment.id));
+    // setAppointments([])
+    // }
+}
 
-  return (
-    <div>AppointmentsAsClient
-
-{ appointments.length > 0 ? 
-      (<div className="cardsContainer">
-        {
-          appointments.map(
-            appointment => {
-              return (
-                  <div
-                    onClick = {() => appointmentSelected(appointment)}
-                    className= "userCardDesign"
-                    key={appointment.id}>
-                    {appointment.date}
-                    
+if (loading) {
+  return <Spinner animation="border" variant="primary" /> ;
+} else if (appointments.length > 0) {
+  return(
+  <>
+    <NavBar/>
+    <div 
+        onClick = {()=>navigate("/createAppointments")}
+        className= "deleteDesign btn-date"
+      >
+      Pedir cita nueva
+    </div>
+    <div className='your-appointments'>Todas tus citas</div>
+    <Container>
+      <Row className='all-appointment'>
+        {appointments.map(appointment => {
+          return (
+            <Col xs={12} sm={12} md={12} lg={12} xl={6} xxl={4} key={appointment.id}>
+              <div className='container-int'>
+                <div
+                  className= "cont-treatment-1"
+                >
+                  <div>
+                  <div className='icon-appointment'><i class="bi bi-journal-text"></i></div>
                   </div>
-              )
-            }
+                  <div className='treatment-line'>
+                  <div className='line-1'>Hora:</div><div>{Moment(appointment.date).format('DD/MM/YYYY HH:mm:ss')}</div>
+                  </div>
+                  <div className='treatment-line'>
+                  <div className='line-1'>Estado:</div><div> {appointment.status}</div>
+                  </div>
+                  <div className='treatment-line'>
+                  <div className='line-1'>Coste:</div><div> {appointment.Treatment.price}</div>
+                  </div>
+                  <div className='treatment-line'>
+                  <div className='line-1'>Tratamiento:</div> <div> {appointment.Treatment.name}</div>
+                  </div>
+                  <div className='treatment-line'>
+                  <div className='line-1'>Duración:</div> <div> {appointment.Treatment.duration}</div>
+                  </div>
+                  
+                  <div className='all-buttons'>
+                    <div
+                    onClick = {() => deleteApp(appointment)}
+                    className= "deleteDesign"
+                  >
+                    Borrar
+                  </div>
+                  <div onClick = {() => appointmentSelected(appointment)} className= "deleteDesign">
+                    Modificar
+                  </div>
+                  </div>
+                </div>
+              </div>
+            </Col>
           )
-
-        }  
-        </div>)
-        
-        :
-
-        ( <Spinner animation="border" variant="primary" />)
-      
-      }
+        })}
+      </Row>
+    </Container>
+    <Footer></Footer>
+    </>
+  )
+} else {
+  return (
+    <div>
+      <div className='date-confirm'>No hay citas</div>
     </div>
   )
+}
 }
